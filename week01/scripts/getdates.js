3d5c35ee8b77cc759690ab4fdb19cb8b34089e2b
dbanda99 <ldavidbandal@gmail.com>
2024-11-04 16:53:24 +0100

    Update 0.0.9

--- week01/scripts/getdates.js
+++ week01/scripts/getdates.js
@@ -1,7 +1,6 @@
-// getdates.js
+document.addEventListener("DOMContentLoaded", function () {
+    const lastModified = new Date(document.lastModified);
+    document.getElementById("last-modified").textContent += lastModified.toLocaleString();
 
-// Set the current year
-document.getElementById('currentyear').textContent = new Date().getFullYear();
+});
 
-// Set the last modified date
-document.getElementById('lastModified').textContent = `Last Modification: ${document.lastModified}`;
